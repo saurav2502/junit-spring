a270766c1146740aeecbe60f49f737a74b2fc4da
saurav kumar <kumar_sauravsk@outlook.com>
2020-02-13 22:37:52 +0530

    Jenkinsfile

--- src/test/java/com/junit/spring/junittest/controller/JunitTestControllerTest.java
+++ src/test/java/com/junit/spring/junittest/controller/JunitTestControllerTest.java
@@ -40,7 +40,7 @@ public class JunitTestControllerTest extends ApplicationTests {
     @Test
     public void createUserTest()throws Exception{
         UserModel model = new UserModel();
-        model.setUserId(985647);
+        model.setUserId(686677);
         model.setUserName("Doing Any");
         model.setUserPassword("Offcource doing");
         ObjectMapper objectMapper = new ObjectMapper();
